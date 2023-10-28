074d8f3ed01c375d47be8b2697b7f02c22db142d
enjaayyy <enujeemontesaarubeosu@gmail.com>
2023-10-28 23:39:28 +0800

    Learning RichText

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(
+    home: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +11,65 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return Container(
+      color: Color(0xFFFCFCFC),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 56,
+              left: 31,
+              child: Container (  
+                width: 49,
+                height: 49,
+                child:Image.asset('images/Vector.png')
+              )
+            ),
+            Positioned(  
+              left: 96,
+              top: 62,
+              child:Container(  
+                width: 113,
+                height: 40,
+                child: RichText(  
+                  text: const TextSpan(
+                    style: TextStyle(  
+                      height: 1.5,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(  
+                        text: 'Welcome Back!',
+                        style: TextStyle(  
+                        fontFamily: 'Gellix',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                        decoration: TextDecoration.none,
+                        color: Colors.black,
+                    ),
+                      ),
+                      TextSpan(
+                        text: 'Monday, 3 October',
+                        style: TextStyle(  
+                          fontFamily: 'Gellix',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          decoration: TextDecoration.none,
+                          color: Color(0xFF9397A0),
+                        )
+
+                  ),
+                    ]   
+                    
+                    ),
+                  ),
+                  
+                ),
+              ),
+            
+
+
+          ],
+        )
     );
+        
   }
 }
